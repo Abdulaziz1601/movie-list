dba5a881044ed3245b777fcced08767ed2e85adf
Abdulaziz <abdulazizfaactory@gmail.com>
2022-04-17 23:04:44 +0500

    some features were added

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import MovieService from '../../services/MovieService';
 import play from '../../resources/play.svg';
@@ -14,22 +14,43 @@ const Movies = (props) => {
     useEffect(() => {
         getAllMovies().then(res => setMovies(res));
     }, []);
+
+    const itemRefs = useRef([]);
+
+    const focusOnItem = (id) => {
+        itemRefs.current.forEach(item => item.classList.remove('movies__block_selected'));
+        itemRefs.current[id].classList.add('movies__block_selected');
+        itemRefs.current[id].focus();
+    }
     
     const renderMovies = (arr) => {
-        return arr.map(movie => (
-            <div className="movies__block" style={{background: `url('${movie.img}') center center/cover no-repeat`}} key={movie.id} onClick={() => props.onMovieSelected(movie.id)}>
-                <div className="movies__info">
-                    <div className='movies__info-inner'>
-                        <img src={play} alt="play btn" className="movies__play" />
-                        <div className="movies__descr">
-                            <div className="movies__name">{ movie.name }</div>
-                            <div className="movies__time">{ movie.release }</div>
+        return arr.map((movie, index) => {
+            if(index === 4) return;
+            const clazz = (index === 0) ? 'movies__block movies__block_selected' : 'movies__block';
+            return (
+                <div
+                    className={clazz}
+                    style={{background: `url('${movie.img}') center center/cover no-repeat`}}
+                    key={movie.id}
+                    onClick={() => {
+                        props.onMovieSelected(movie.id);
+                        focusOnItem(index);
+                    }}
+                    ref={el => itemRefs.current[index] = el}
+                >
+                    <div className="movies__info">
+                        <div className='movies__info-inner'>
+                            <img src={play} alt="play btn" className="movies__play" />
+                            <div className="movies__descr">
+                                <div className="movies__name">{ movie.name }</div>
+                                <div className="movies__time">{ movie.release }</div>
+                            </div>
                         </div>
+                        <div className="movies__duration">{ movie.duration }</div>
                     </div>
-                    <div className="movies__duration">{ movie.duration }</div>
                 </div>
-            </div>
-        ))
+            )
+        })
     }
     const items = renderMovies(movies);
     return (
